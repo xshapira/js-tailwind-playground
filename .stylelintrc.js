6c7337e6eb808fc34503bfb05e13f907006b8d1f
Max Shapira <maxshapira9@gmail.com>
2022-06-10 10:27:43 +0300

    remove stylelint-prettier packages to avoid linting conflicts

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,11 +1,13 @@
 module.exports = {
-  extends: [
-    'stylelint-config-standard',
-    'stylelint-config-rational-order',
-    'stylelint-prettier/recommended'
-  ],
+  extends: ['stylelint-config-standard', 'stylelint-config-rational-order'],
   plugins: ['stylelint-order', 'stylelint-scss'],
   rules: {
-    'at-rule-no-unknown': null
+    'at-rule-no-unknown': [
+      null,
+      {
+        ignoreAtRules: ['/regex/', 'string']
+      }
+    ],
+    'no-invalid-position-at-import-rule': null
   }
 };
